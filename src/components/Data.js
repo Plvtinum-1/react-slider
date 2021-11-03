6af66d6e633f2f19fe6032b323fd86e619c862b5
Ismail Dahri <dahriiismail@gmail.com>
2021-11-03 12:38:11 +0100

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        image: 'https://images.unsplash.com/photo-1635882009498-51b584c0fe15?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1738&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1634674959285-ccb604b57137?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1620647910715-5aa3e4d759a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+    }, 
+    {
+        image: 'https://images.unsplash.com/photo-1610805177214-885738d255f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80'
+    }
+]
+
+export default data;
--- src/components/ImgSlide.jsx
+++ src/components/ImgSlide.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react';
+import ArrowRight from '@mui/icons-material/ArrowForwardIos';
+import ArrowLeft from '@mui/icons-material/ArrowBackIosNew';
 
-const ImgSlide = () => {
+const ImgSlide = ({ slides }) => {
+    const [current, setCurrent] = useState(0);
+    const nextSlide = () => {
+
+        setCurrent(current === slides.length - 1 ? 0 : current + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? slides.length - 1 : current - 1)
+    }
+
+    if(!Array.isArray(slides) || slides.length <= 0){
+        return null
+    }
     return (
-        <div>
-            
-        </div>
+        <section className='slider'>
+        <ArrowLeft className='left' onClick={prevSlide} />
+        <ArrowRight className='right' onClick={nextSlide} />
+         {slides.map((item, index) => (
+             <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                 {index === current && (
+                     <img className='image' src={item.image} alt="img" />
+                 )}
+             </div>
+         ))}   
+        </section>
     )
 }
 
